e2d29875b0c70a6beb59e657ceb4dab23b2c9a3c
Jonas Maihöfer <jonas.maihoefer@about-software.de>
2025-06-20 13:37:52 +0200

    fix: test

--- src/run.py
+++ src/run.py
@@ -3,6 +3,7 @@ import sys
 USE_CUPY = True  # os.getenv("USE_CUPY") == "1"    # or set to "true", however you like
 
 if USE_CUPY:
+    print("start importing cupy")
     import cupy
     # Tell Python: whenever someone does `import numpy`, give them `cupy` instead
     sys.modules["numpy"] = cupy
