e96721c0dd52e343eb23525d7c321b058afaf600
Jonas Maihöfer <jonas.maihoefer@about-software.de>
2025-06-24 11:11:55 +0200

    fix: fix activation order

--- start.sh
+++ start.sh
@@ -12,19 +12,21 @@ export USECUDA=1
 # load cuda package
 spack load cuda@12.6.2
 
-# 1) Ask Spack where CUDA@12.6.2 lives
+# Ask Spack where CUDA@12.6.2 lives
 CUDA_PREFIX=$(spack location -i cuda@12.6.2)
 
-# 2) Check that this is the right directory
+# Check that this is the right directory
 echo "CUDA prefix: $CUDA_PREFIX"
 ls $CUDA_PREFIX/lib64/libnvrtc.so*
+
+# Activate virtual environment
+source venv/bin/activate
 
 # 3) Export it into LD_LIBRARY_PATH (do this after you activate your venv)
 export LD_LIBRARY_PATH=$CUDA_PREFIX/lib64:$LD_LIBRARY_PATH
 
 # 4) Confirm
-echo $LD_LIBRARY_PATH
+echo "LD Library Path: $LD_LIBRARY_PATH"
 
-# Activate virtual environment and run your application
-source venv/bin/activate
+# run application
 sh run.sh
