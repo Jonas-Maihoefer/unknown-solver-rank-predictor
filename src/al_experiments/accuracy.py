34aa416ee9c1c065c71a8ef420e48ad1637107c2
Jonas Maihöfer <jonas.maihoefer@about-software.de>
2025-06-27 15:00:00 +0200

    fix: another adjustment

--- src/al_experiments/accuracy.py
+++ src/al_experiments/accuracy.py
@@ -125,7 +125,7 @@ class Accuracy:
         #print(similarity)
         #print(similarity.shape)
 
-        score = total_added_runtime #similarity + self.rt_weight * total_added_runtime
+        score = similarity + self.rt_weight * total_added_runtime
         #print("fast")
         #for sc in score:
         #    print(sc, end=", ")
