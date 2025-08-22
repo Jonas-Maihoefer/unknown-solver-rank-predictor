f44960d48fb4e855fd096c21240de26c0913ced6
Jonas Maihöfer <jonas.maihoefer@about-software.de>
2025-08-22 17:32:43 +0200

    fix: fix score renaming

--- src/al_experiments/accuracy.py
+++ src/al_experiments/accuracy.py
@@ -152,7 +152,7 @@ class Accuracy:
         if bad_indices.size > 0:
             for bad_idx in bad_indices:
                 bad_idx = self.instance_idx[remaining_mask][bad_idx]
-                print(f"135000000 / ({similarity[bad_idx]} * {total_added_runtime[bad_idx]}) = {135000000 / (similarity[bad_idx] * total_added_runtime[bad_idx])}")
+                print(f"135000000 / ({score[bad_idx]} * {total_added_runtime[bad_idx]}) = {135000000 / (similarity[bad_idx] * total_added_runtime[bad_idx])}")
                 print(new_pred[bad_idx])
                 print(f"total_added_runtime is ({self.number_of_reduced_solvers} - {thresholds[bad_idx]}) * ({next_solver[rt][bad_idx]} - {current_solver[rt][bad_idx]}) = {total_added_runtime[bad_idx]}")
 
